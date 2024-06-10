687e0a7ac833d19d24a7ef8f8aa37e44e9ce2dd7
roushan-sheik <roushansheik@gmail.com>
2024-06-10 13:25:05 +0600

    jwt done

--- app.js
+++ app.js
@@ -15,22 +15,42 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(cookieParser());
-// verify token middleware
-const verifyToken = (req, res, next) => {
-  const token = req.cookies?.token;
+app.use( cookieParser() );
 
-  if (!token) {
-    return res.status(401).send({ message: "unauthorized access" });
-  }
-  if (token) {
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if (err) res.send(401).send({ message: "unauthorized access" });
-      req.user = decoded;
+
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
+
+    // middlewares 
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+    // use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
       next();
-    });
-  }
-};
+    }
 // Home and health route
 app.get("/", (req, res) => {
   res.send("Hello BitCraft");
